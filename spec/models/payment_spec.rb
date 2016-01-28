9e2da224a549d196d186d6cbec2a629018275a29
Razevich <razevich.c@gmail.com>
2016-01-27 19:54:53 -0600

    add test for assoc on payment model

--- spec/models/payment_spec.rb
+++ spec/models/payment_spec.rb
@@ -2,4 +2,11 @@ require 'rails_helper'
 
 RSpec.describe Payment, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
+
+  context "Setting associations" do
+    it "Should belong to LOAN" do
+      t = Payment.reflect_on_association(:loan)
+      t.macro.should == :belongs_to
+    end
+  end
 end
