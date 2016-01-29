72488c660c928a403b1d345f356bc73eb7511291
Razevich <razevich.c@gmail.com>
2016-01-29 15:57:02 -0600

    finish last test, chang depricated test to expext

--- spec/models/loan_spec.rb
+++ spec/models/loan_spec.rb
@@ -1,7 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe Loan, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
 
   context "Set outstanding balance" do
     let(:loan) { Loan.create!(funded_amount: 100.0) }
